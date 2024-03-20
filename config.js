58c01b822e88a35bdd1ece169caf77afd5700f6f
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2024-03-20 20:02:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'Trang Nhung';
+const eventName = 'International Day of Happiness';
+const titleCard = 'Tặng Thế Giới Của Tớ ❤️ ';
+const contentCard = 'ngày mà mặt trời chạy ngang qua đường xích đạo nên ngày và đêm dài bằng nhau đó là biểu tượng của sự cần bằng và hài hòa của vũ trụ và đó cũng là biểu tượng của sự hạnh phúc . Lúc đầu tớ nghĩ gặp cậu là một sự tình cờ ,nhưng cậu biết không có lẽ tớ đã giành hết may mắn vào sự tình cờ này .Bởi vì tớ đã từng một mình rất lâu trước đó,nên việc cậu xuất hiện như thế là một điều dịu dàng biết bao trong thế giới hàng tỷ người, giữa hàn vạn nỗi đau.Nếu có thể, tớ muốn hạnh phúc của tớ.Có Cậu ở trong đó.Chúc cậu 20/3 thật hạnh phúc ❤️ ';
+
+const giftImage = 'trangnhung.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
